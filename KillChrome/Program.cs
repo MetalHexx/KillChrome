164a93345db8087276900957d161c6609a32ee78
William Pereira <MetalHexx@outlook.com>
2020-02-29 01:02:23 -0500

    Added a surprise

--- KillChrome/Program.cs
+++ KillChrome/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,9 @@ namespace KillChrome
         static void Main(string[] args)
         {
             Process.Start(@"cmd", @"/c taskkill.exe /im:chrome.exe /f");
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = Environment.CurrentDirectory + "\\screaming-goat.wav";
+            player.PlaySync();
         }
     }
 }
